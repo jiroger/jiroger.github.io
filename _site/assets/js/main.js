c2a5b150b1627e7f7c54b46df245fcaea1027e3c
roger ji <roger.ji.32021@gmail.com>
2019-06-07 15:54:12 -0700

    added some info to about; favicon fix (?); need to address windows issue

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,12 +1,3 @@
-//sets up google analytics
-window.ga = function() {
-  ga.q.push(arguments)
-};
-ga.q = [];
-ga.l = +new Date;
-ga('create', 'UA-140938828-1', 'auto');
-ga('set', 'transport', 'beacon');
-ga('send', 'pageview')
 
 //takes care of the toggle button
 $('#sidebarCollapse, #topbarCollapse').on('click',
@@ -31,3 +22,13 @@ $(document).ready(function() {
   //date this page was last modified
   document.getElementById("lastModified").innerHTML = 'Last modified: ' +  document.lastModified;
 });
+
+//sets up google analytics
+window.ga = function() {
+  ga.q.push(arguments)
+};
+ga.q = [];
+ga.l = +new Date;
+ga('create', 'UA-140938828-1', 'auto');
+ga('set', 'transport', 'beacon');
+ga('send', 'pageview')
